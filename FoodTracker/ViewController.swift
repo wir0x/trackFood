78fdcf745e291e9cde90101d2d81177ee1103282
Gonzalo Salazar Velasquez <gonzalosalazarvelasquez@Gonzalos-MacBook-Pro.local>
2015-12-05 09:09:31 -0400

    connect ui with code

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
